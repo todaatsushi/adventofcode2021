06acf36b6bd366d2b77362ef31e7c1c125853852
Atsushi Toda <todaatsushi0@gmail.com>
2021-12-26 19:38:18 +0000

    Solve the problem

---
+++ aoc1/src/app.ts
@@ -0,0 +1,6 @@
+import { testInputs, puzzleInputs } from './inputs'
+import getIncrementCountInNums from './getCount'
+
+console.log(
+  `Answer for advent of code #1 is ${getIncrementCountInNums(puzzleInputs)}`
+)
