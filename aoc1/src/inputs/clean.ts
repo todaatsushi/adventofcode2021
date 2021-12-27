3c4ecd0f8cf8ccc4fc8020fa127d295f409eb7f3
Atsushi Toda <todaatsushi0@gmail.com>
2021-12-27 14:29:35 +0000

    Make this look nice

--- aoc1/src/inputs/clean.ts
+++ aoc1/src/inputs/clean.ts
@@ -1,3 +1,10 @@
-const getInput = (input: string): number[] => input.split("\n").map((num: string) => parseInt(num)).filter((num: number) => num !== NaN)
+const getInput = (input: string): number[] =>
+  input
+    .split("\n")
+    .map((num: string): string => num.trim())
+    .filter((num: string): boolean => num !== '')
+    .map((num: string) => parseInt(num))
+    .filter((num: number | typeof NaN) => typeof num === 'number')
+
 
 export default getInput
