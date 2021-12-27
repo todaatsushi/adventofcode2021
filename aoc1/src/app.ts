3a3e1a0c929417b35af61c81598417b66c75ca52
Atsushi Toda <todaatsushi0@gmail.com>
2021-12-27 14:44:40 +0000

    Solve part 2

--- aoc1/src/app.ts
+++ aoc1/src/app.ts
@@ -1,6 +1,11 @@
 import { part1TestInputs, part1PuzzleInputs } from './inputs/inputs'
 import getIncrementCountInNums from './getCount'
+import getDataWithWindow from './getData'
 
 console.log(
-  `Part 1 : answer for advent of code #1 is ${getIncrementCountInNums(part1PuzzleInputs)}`
+  `Part 1 : answer for advent of code #1 is ${getIncrementCountInNums(part1PuzzleInputs)}`,
+)
+
+console.log(
+  `Part 2 : answer for advent of code #1 is ${getIncrementCountInNums(getDataWithWindow(part1PuzzleInputs, 3))}`
 )
