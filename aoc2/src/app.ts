27c8eeaa2fdb7635e16bbc44a83bab19dedce7ee
Atsushi Toda <todaatsushi0@gmail.com>
2021-12-28 18:43:23 +0000

    Solve part 2

--- aoc2/src/app.ts
+++ aoc2/src/app.ts
@@ -1,6 +1,10 @@
 import rawPuzzleInput from './inputs/puzzle'
 import getPosition from './data/getPosition'
+import getPositionWithAim from './data/getPositionWithAim'
 
 console.log(
-  `AOC2: answer: ${getPosition(rawPuzzleInput)}!`
+  `AOC2: part 1 answer: ${getPosition(rawPuzzleInput)}!`
+)
+console.log(
+  `AOC2: part 2 answer: ${getPositionWithAim(rawPuzzleInput)}!`
 )
