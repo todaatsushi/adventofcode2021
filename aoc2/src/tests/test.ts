6199b3245b2972005f27790103424a6ad421375a
Atsushi Toda <todaatsushi0@gmail.com>
2021-12-28 18:41:29 +0000

    Add tests from part 2 to tests

--- aoc2/src/tests/test.ts
+++ aoc2/src/tests/test.ts
@@ -1,3 +1,12 @@
-import testPart1 from "./part1"
+import testPart1 from './part1'
+import testPart2 from './part2'
 
-(() => testPart1())()
+(
+  () => {
+    console.log('Testing part 1:')
+    testPart1()
+
+    console.log('\nTesting part 2:')
+    testPart2()
+  }
+)()
