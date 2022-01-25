03171ae015ffecdd7110dfd755deedb50b09e6e6
Atsushi Toda <todaatsushi0@gmail.com>
2022-01-25 15:49:29 +0000

    Move test input out of the class for reuse

--- aoc3/tests.py
+++ aoc3/tests.py
@@ -1,24 +1,25 @@
 import unittest
 
-from adapter import InputAdapter
+import adapters
+
+TEST_INPUT = (
+    "00100\n"
+    "11110\n"
+    "10110\n"
+    "10111\n"
+    "10101\n"
+    "01111\n"
+    "00111\n"
+    "11100\n"
+    "10000\n"
+    "11001\n"
+    "00010\n"
+    "01010\n"
+)
 
 
 class InputTestCase(unittest.TestCase):
     def setUp(self):
-        self.TEST_INPUT = (
-            "00100\n"
-            "11110\n"
-            "10110\n"
-            "10111\n"
-            "10101\n"
-            "01111\n"
-            "00111\n"
-            "11100\n"
-            "10000\n"
-            "11001\n"
-            "00010\n"
-            "01010\n"
-        )
         self.TEST_OUTPUT = (
             "00100",
             "11110",
