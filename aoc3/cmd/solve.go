96b33bd775fdde8b3c0f6e3f32451afd85dae92b
Atsushi Toda <todaatsushi0@gmail.com>
2022-05-17 17:31:11 +0100

    aoc3: init solve command that takes filename

---
+++ aoc3/cmd/solve.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// solveCmd represents the solve command
+var solveCmd = &cobra.Command{
+	Use:   "solve",
+	Short: "A brief description of your command",
+	Run: func(cmd *cobra.Command, args []string) {
+		flags := cmd.Flags()
+		filename, err := flags.GetString("file")
+		if filename == "" {
+			log.Fatal("Couldn't get filename:", filename, err)
+		}
+		getPowerConsumption(filename)
+	},
+}
+
+func getPowerConsumption(filename string) {
+	fmt.Println("Solve using:", filename)
+}
+
+func init() {
+	rootCmd.AddCommand(solveCmd)
+
+	flags := solveCmd.Flags()
+	flags.StringP("file", "f", "", "Pass input file to get the power consumption.")
+}
