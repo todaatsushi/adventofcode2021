981a74e77319fc6677134dde825ee6f0313f1a5e
Atsushi Toda <todaatsushi0@gmail.com>
2022-06-09 18:38:50 +0100

    Funcs to play bingo

--- aoc4/internal/aoc4/play.go
+++ aoc4/internal/aoc4/play.go
@@ -17,3 +17,21 @@ func getBoards(inputs []string) []*Board {
 	wg.Wait()
 	return boards
 }
+
+func checkBoards(toCheck int, boards []*Board) {
+	wg := sync.WaitGroup{}
+	for _, board := range boards {
+		wg.Add(1)
+		go func(board *Board, val int) {
+			defer wg.Done()
+			board.check(val)
+		}(board, toCheck)
+	}
+	wg.Wait()
+}
+
+func makeCalls(boards []*Board, calls chan int) {
+	for call := range calls {
+		checkBoards(call, boards)
+	}
+}
