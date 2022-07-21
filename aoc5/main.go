48f5b3a7ee0edab47d366498acc1a5a7d7fb851d
Atsushi Toda <todaatsushi0@gmail.com>
2022-07-21 18:20:29 +0100

    Read and create line

--- aoc5/main.go
+++ aoc5/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Line struct {
+	startX int
+	startY int
+	endX   int
+	endY   int
+}
+
+func getXAndY(lineStr string) (int, int) {
+	coords := strings.Split(lineStr, ",")
+
+	strX := strings.TrimSpace(coords[0])
+	strY := strings.TrimSpace(coords[1])
+	intX, _ := strconv.Atoi(strX)
+	intY, _ := strconv.Atoi(strY)
+
+	return intX, intY
+}
+
+func newLine(inputs string) Line {
+	parts := strings.Split(inputs, "->")
+	startX, startY := getXAndY(parts[0])
+	endX, endY := getXAndY(parts[1])
+	return Line{startX: startX, startY: startY, endX: endX, endY: endY}
+}
+
+func readInput(filename string) []string {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal("Couldn't read file:", filename)
+	}
+	rawInput := string(content)
+	rawInput = strings.TrimSpace(rawInput)
+	input := strings.Split(rawInput, "\n")
+
+	return input
+}
 
 func main() {
-	fmt.Print("Hello")
+	filename := os.Args[1]
+
+	input := readInput(filename)
+	for _, l := range input {
+		fmt.Println(newLine(l))
+	}
 }
