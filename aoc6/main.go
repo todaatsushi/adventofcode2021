5ad3b8d3c7cf42bab0a0886a5b1bf88821f530be
Atsushi Toda <todaatsushi0@gmail.com>
2022-07-23 00:04:05 +0100

    Solve part 1

--- aoc6/main.go
+++ aoc6/main.go
@@ -4,10 +4,17 @@ import (
 	"adventofcode2021/aoc6/src"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	filename := os.Args[1]
-	fishAges := src.ReadInput(filename)
-	fmt.Println(fishAges)
+	daysStr := os.Args[2]
+	days, _ := strconv.Atoi(daysStr)
+
+	fishAges := src.GetFishAgesFromInput(filename)
+	fish := src.FishFromAges(fishAges)
+
+	total := src.GetNumFishAfterDays(fish, days)
+	fmt.Println("Num of fish:", total)
 }
