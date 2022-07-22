389a500c771bc4cc599aa8eeabb426f2a6477c71
Atsushi Toda <todaatsushi0@gmail.com>
2022-07-23 00:02:40 +0100

    Get init fishes from input

--- aoc6/src/fish.go
+++ aoc6/src/fish.go
@@ -7,3 +7,14 @@ type Fish struct {
 func createFish(internalTimer int) *Fish {
 	return &Fish{internalTimer: internalTimer}
 }
+
+func FishFromAges(timers []int) []*Fish {
+	fish := []*Fish{}
+
+	var created *Fish
+	for _, timer := range timers {
+		created = createFish(timer)
+		fish = append(fish, created)
+	}
+	return fish
+}
