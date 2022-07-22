0c6df2a8bff0a32ffd4e1734a7fac48d2a7f1443
Atsushi Toda <todaatsushi0@gmail.com>
2022-07-23 00:03:05 +0100

    Play the game - age the fish as days elapse and create new fish if needed'

--- aoc6/src/fish.go
+++ aoc6/src/fish.go
@@ -4,6 +4,17 @@ type Fish struct {
 	internalTimer int
 }
 
+func (f *Fish) age() bool {
+	create := false
+	if f.internalTimer == 0 {
+		f.internalTimer = 6
+		create = true
+	} else {
+		f.internalTimer--
+	}
+	return create
+}
+
 func createFish(internalTimer int) *Fish {
 	return &Fish{internalTimer: internalTimer}
 }
@@ -17,4 +28,26 @@ func FishFromAges(timers []int) []*Fish {
 		fish = append(fish, created)
 	}
 	return fish
+}
+
+func GetNumFishAfterDays(fish []*Fish, days int) int {
+	var create bool
+	var newFish *Fish
+	elapsed := 0
+
+	for {
+		if days == elapsed {
+			break
+		}
+		for _, f := range fish {
+			create = f.age()
+			if create == true {
+				newFish = createFish(8)
+				fish = append(fish, newFish)
+			}
+		}
+		elapsed++
+		create = false
+	}
+	return len(fish)
 }
