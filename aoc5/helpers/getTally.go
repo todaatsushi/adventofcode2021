1139b5792b8f945158a10258f75dfa3a0cfcb321
Atsushi Toda <todaatsushi0@gmail.com>
2022-07-22 22:36:47 +0100

    Split straight line logic to it's own func

--- aoc5/helpers/getTally.go
+++ aoc5/helpers/getTally.go
@@ -1,30 +1,33 @@
 package helpers
 
+func tallyStraightLine(line *Line, tally *int, graph *Graph) {
+	var start, end int
+	if line.StartX == line.EndX {
+		start, end = line.GetStartAndEnd("y")
+		for i := start; i <= end; i++ {
+			graph.Points[line.StartX][i]++
+			if graph.Points[line.StartX][i] == 2 {
+				*tally++
+			}
+		}
+	} else {
+		start, end = line.GetStartAndEnd("x")
+		for i := start; i <= end; i++ {
+			graph.Points[i][line.StartY]++
+			if graph.Points[i][line.StartY] == 2 {
+				*tally++
+			}
+		}
+	}
+}
+
 func GetTally(lines []Line) int {
 	tally := 0
 	graph := *NewGraph(lines)
 
-	var start int
-	var end int
 	for _, l := range lines {
 		if l.IsStraight() == true {
-			if l.StartX == l.EndX {
-				start, end = l.GetStartAndEnd("y")
-				for i := start; i <= end; i++ {
-					graph.Points[l.StartX][i]++
-					if graph.Points[l.StartX][i] == 2 {
-						tally++
-					}
-				}
-			} else {
-				start, end = l.GetStartAndEnd("x")
-				for i := start; i <= end; i++ {
-					graph.Points[i][l.StartY]++
-					if graph.Points[i][l.StartY] == 2 {
-						tally++
-					}
-				}
-			}
+			tallyStraightLine(&l, &tally, &graph)
 		}
 	}
 	return tally
