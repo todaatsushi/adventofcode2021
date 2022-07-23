4a591a82fd1e15042d018835bdd06251c90c7b0b
Atsushi Toda <todaatsushi0@gmail.com>
2022-07-23 18:57:53 +0100

    Find smallest number of moves

--- aoc7/main.go
+++ aoc7/main.go
@@ -37,7 +37,34 @@ func createLocationMap(crabs []int) map[int]int {
 		lm[c]++
 	}
 	return lm
+}
 
+func absDiffInt(x, y int) int {
+	if x > y {
+		return x - y
+	}
+	return y - x
+}
+
+func findOptimalConversionPoint(locationMap *map[int]int, start int, end int) int {
+	smallestMoves := math.MaxInt
+
+	for point := start; point < end+1; point++ {
+		total := 0
+		for location, crabCount := range *locationMap {
+			if location == point {
+				continue
+			}
+			distance := absDiffInt(point, location)
+			total += distance * crabCount
+		}
+
+		if smallestMoves > total {
+			smallestMoves = total
+		}
+	}
+
+	return smallestMoves
 }
 
 func main() {
