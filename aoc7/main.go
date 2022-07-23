f7e331a11108dfec10c04e0cc8e0c9446292c8c0
Atsushi Toda <todaatsushi0@gmail.com>
2022-07-23 18:13:26 +0100

    Read file from input

--- aoc7/main.go
+++ aoc7/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+func parseCrabsFromFile(filename string) []int {
+	content, _ := os.ReadFile(filename)
+	rawContent := string(content)
+	positions := strings.Split(rawContent, ",")
+
+	crabs := make([]int, len(positions))
+	wg := sync.WaitGroup{}
+	for i, p := range positions {
+		wg.Add(1)
+		p = strings.TrimSpace(p)
+		go func(crabs *[]int, index int, val string) {
+			defer wg.Done()
+			asNum, _ := strconv.Atoi(val)
+			(*crabs)[index] = asNum
+		}(&crabs, i, p)
+	}
+	wg.Wait()
+
+	sort.Ints(crabs)
+	return crabs
+}
 
 func main() {
-	fmt.Println("aoc7")
+	filename := os.Args[1]
+	crabs := parseCrabsFromFile(filename)
+
+	fmt.Println(crabs)
 }
