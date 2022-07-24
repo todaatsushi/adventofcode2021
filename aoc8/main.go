1946341db7d9ae464e116d99eac3c621226baf71
Atsushi Toda <todaatsushi0@gmail.com>
2022-07-24 19:52:40 +0100

    Read file + fetch outputs

--- aoc8/main.go
+++ aoc8/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func readEntry(entry string) []string {
+	splitContent := strings.Split(entry, "|")
+
+	rawOutput := strings.TrimSpace(splitContent[1])
+	splitRawOutput := strings.Split(rawOutput, " ")
+
+	output := make([]string, 4)
+	for i, o := range splitRawOutput {
+		output[i] = strings.TrimSpace(o)
+	}
+	return output
+
+}
+
+func readInput(filename string) [][]string {
+	content, _ := os.ReadFile(filename)
+
+	strContent := string(content)
+	strContent = strings.TrimSpace(strContent)
+
+	rawEntries := strings.Split(strContent, "\n")
+	entries := make([][]string, len(rawEntries))
+
+	for i, e := range rawEntries {
+		e = strings.TrimSpace(e)
+		entries[i] = readEntry(e)
+	}
+	return entries
+}
 
 func main() {
-	fmt.Println("Hello world")
+	filename := os.Args[1]
+	output := readInput(filename)
+	fmt.Println(output)
 }
