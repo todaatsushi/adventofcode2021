75f5905a6b418ed222d1e3a95d9cb6323eb7a75c
Atsushi Toda <todaatsushi0@gmail.com>
2022-09-05 23:26:09 +0100

    aoc8: solve

--- aoc8/main.go
+++ aoc8/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"adventofcode2021/aoc8/part1"
+	"adventofcode2021/aoc8/part2"
 	"fmt"
 	"os"
 )
@@ -11,4 +12,7 @@ func main() {
 	part1Answer := part1.Solve(filename)
 
 	fmt.Println("part 1:", part1Answer)
+
+	part2Answer := part2.Solve(filename)
+	fmt.Println("part 2:", part2Answer)
 }
