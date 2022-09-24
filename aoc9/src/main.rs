f98257311c4bf7f14df6bdefacc5904d50155797
Atsushi Toda <todaatsushi0@gmail.com>
2022-09-24 17:31:33 +0100

    parse input for files

--- aoc9/src/main.rs
+++ aoc9/src/main.rs
@@ -1,3 +1,23 @@
+fn parse_input(test: bool) -> Vec<Vec<u16>> {
+    let file_contents = match test {
+        true => include_str!("../inputs/test.txt").lines(),
+        false => include_str!("../inputs/puzzle.txt").lines(),
+    };
+    let mut points: Vec<Vec<u16>> = Vec::new();
+
+    for line in file_contents {
+        let line_points = line
+            .split("")
+            .filter(|&height| !height.is_empty())
+            .map(|x| x.trim().parse::<u16>().unwrap())
+            .collect();
+
+        let _ = &points.push(line_points);
+    }
+
+    points
+}
 fn main() {
-    println!("Hello, world!");
+    let input = parse_input(true);
+    println!("{:?}", input);
 }
