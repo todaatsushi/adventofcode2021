38b6287d01e78f6f9c03023c3dd0e67838ad8a44
Atsushi Toda <todaatsushi0@gmail.com>
2022-09-25 16:55:33 +0100

    Function that determines if a you should add a point
    Yes, this is shit - it should use some recursion and memoisation i think but cba

--- aoc9/src/main.rs
+++ aoc9/src/main.rs
@@ -17,6 +17,30 @@ fn parse_input(test: bool) -> Vec<Vec<i32>> {
 
     points
 }
+
+fn should_add_point(x: i32, y: i32, grid: &Vec<Vec<i32>>, moves: &[[i32; 2]; 4]) -> bool {
+    let mut add = true;
+    let num_rows = grid.len() as i32;
+    let num_cols = grid[0].len() as i32;
+    let current = grid[y as usize][x as usize];
+
+    for alter in moves.iter() {
+        let row = y + alter[0];
+        let col = x + alter[1];
+
+        let row_in_range = row < num_rows && row >= 0;
+        let col_in_range = col < num_cols && col >= 0;
+
+        if row_in_range && col_in_range {
+            let adjacent_value = grid[row as usize][col as usize];
+            if current >= adjacent_value {
+                add = false;
+            }
+        }
+    }
+    add
+}
+
 fn main() {
     let input = parse_input(true);
     println!("{:?}", input);
