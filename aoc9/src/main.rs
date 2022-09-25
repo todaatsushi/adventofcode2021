5058043593e8f1c8a2d87ab132105b238b67bae7
Atsushi Toda <todaatsushi0@gmail.com>
2022-09-25 18:19:23 +0100

    WIP: part 2 init file and call

--- aoc9/src/main.rs
+++ aoc9/src/main.rs
@@ -1,7 +1,9 @@
 use aoc9::part1::solve_part_1;
+use aoc9::part2::solve_part_2;
 use aoc9::utils::is_test;
 
 fn main() {
     let test = is_test().unwrap();
     solve_part_1(test);
+    solve_part_2(test);
 }
---
+++ aoc9/src/part2.rs
@@ -0,0 +1,7 @@
+use crate::utils::parse_input;
+
+pub fn solve_part_2(is_test: bool) {
+    let grid = parse_input(is_test);
+
+    println!("{:?}", grid);
+}
