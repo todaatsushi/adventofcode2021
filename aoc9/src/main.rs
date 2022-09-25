849e18b4f1bc3aaf50db0916fe98d170d2226915
Atsushi Toda <todaatsushi0@gmail.com>
2022-09-25 16:57:43 +0100

    Solve part 1

--- aoc9/src/main.rs
+++ aoc9/src/main.rs
@@ -42,6 +42,19 @@ fn should_add_point(x: i32, y: i32, grid: &Vec<Vec<i32>>, moves: &[[i32; 2]; 4])
 }
 
 fn main() {
-    let input = parse_input(true);
-    println!("{:?}", input);
+    let grid = parse_input(true);
+    let rows = grid.len() as i32;
+    let cols = grid[0].len() as i32;
+    let mut risk_score = 0;
+    let directions = [[1, 0], [-1, 0], [0, -1], [0, 1]];
+
+    for row_index in 0..rows {
+        for col_index in 0..cols {
+            let should_add = should_add_point(col_index, row_index, &grid, &directions);
+            if should_add {
+                risk_score += &grid[row_index as usize][col_index as usize] + 1;
+            }
+        }
+    }
+    println!("{}", risk_score);
 }
