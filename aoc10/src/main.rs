bde495b8fc0f89fdb08a2ee6d64c1a8eae5a7710
Atsushi Toda <todaatsushi0@gmail.com>
2022-09-30 17:02:01 +0100

    Init mappers
    Brackets to know which closers should be expected in the string and score mappers to help track the scores

--- aoc10/src/main.rs
+++ aoc10/src/main.rs
@@ -1,4 +1,25 @@
 use aoc10::utils;
+use std::collections::{HashMap, HashSet};
+
+fn build_closing_bracket_mapper() -> HashMap<&'static str, &'static str> {
+    let mut map = HashMap::new();
+
+    map.insert("{", "}");
+    map.insert("[", "]");
+    map.insert("(", ")");
+    map.insert("<", ">");
+    map
+}
+
+fn build_score_mapper() -> HashMap<&'static str, i32> {
+    let mut map = HashMap::new();
+
+    map.insert(")", 3);
+    map.insert("]", 57);
+    map.insert("}", 1197);
+    map.insert(">", 25137);
+    map
+}
 
 fn main() {
     let test = utils::is_test().unwrap();
