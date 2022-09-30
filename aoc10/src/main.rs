884b8c3d3eb5b9dea73a6795ff9a4ecf681de193
Atsushi Toda <todaatsushi0@gmail.com>
2022-09-30 17:03:10 +0100

    Solve part 1

--- aoc10/src/main.rs
+++ aoc10/src/main.rs
@@ -24,5 +24,41 @@ fn build_score_mapper() -> HashMap<&'static str, i32> {
 fn main() {
     let test = utils::is_test().unwrap();
     let input = utils::parse_input(test);
-    println!("{:?}", input);
+
+    let closing_mapper = build_closing_bracket_mapper();
+    let score_mapper = build_score_mapper();
+    let opening_brackets = HashSet::from(["{", "(", "<", "["]);
+
+    let mut expected: &str;
+    let mut total = 0;
+
+    for line in input {
+        let mut bracket_stack: Vec<&str> = Vec::new();
+        let mut first_illegal_character: Option<&str> = None;
+        let mut subtotal = 0;
+
+        for bracket in line {
+            if opening_brackets.contains(bracket) {
+                bracket_stack.push(closing_mapper.get(bracket).unwrap());
+            } else {
+                expected = match bracket_stack.pop() {
+                    Some(b) => b,
+                    None => panic!("bracket_stack empty"),
+                };
+                if expected != bracket {
+                    first_illegal_character = match first_illegal_character {
+                        Some(value) => Some(value),
+                        None => Some(bracket),
+                    };
+
+                    if first_illegal_character.unwrap() == bracket {
+                        subtotal += score_mapper.get(bracket).unwrap()
+                    }
+                }
+            }
+        }
+
+        total += subtotal;
+    }
+    println!("Total: {}", total);
 }
