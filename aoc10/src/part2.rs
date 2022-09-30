1d9a2bb997120a606b14fccba3734ecbdd5e17b5
Atsushi Toda <todaatsushi0@gmail.com>
2022-09-30 18:14:41 +0100

    Solve part 2: keep scores and tally them when lines are incomplete

--- aoc10/src/part2.rs
+++ aoc10/src/part2.rs
@@ -14,8 +14,11 @@ fn build_score_mapper() -> HashMap<&'static str, u128> {
 pub fn solve_part_2(input: &Vec<Vec<&'static str>>) {
     let closing_mapper = utils::build_closing_bracket_mapper();
     let opening_brackets = HashSet::from(["{", "(", "<", "["]);
+    let score_mapper = build_score_mapper();
 
     let mut expected: &str;
+
+    let mut scores: Vec<u128> = Vec::new();
 
     for line in input {
         let mut bracket_stack: Vec<&str> = Vec::new();
@@ -39,6 +42,20 @@ pub fn solve_part_2(input: &Vec<Vec<&'static str>>) {
             }
         }
 
-        if incomplete {}
+        if incomplete {
+            let mut total = 0;
+            let mut latest: Option<&str> = bracket_stack.pop();
+
+            while let Some(bracket) = latest {
+                total *= 5;
+                total += score_mapper.get(bracket).unwrap();
+
+                latest = bracket_stack.pop();
+            }
+            scores.push(total);
+        }
     }
+    scores.sort();
+    let index = scores.len() / 2;
+    println!("Part 2 answer: {}", scores[index]);
 }
