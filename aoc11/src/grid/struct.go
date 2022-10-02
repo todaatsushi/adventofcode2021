c6194eb88d01737b7f8b027d899d352b29a82ddb
Atsushi Toda <todaatsushi0@gmail.com>
2022-10-02 17:06:11 +0100

    Add display method to show the grid

--- aoc11/src/grid/struct.go
+++ aoc11/src/grid/struct.go
@@ -1,7 +1,22 @@
 package grid
 
+import (
+	"fmt"
+)
+
 type Grid struct {
 	Board               [10][10]int
 	Flashes             int
 	coordinateModifiers [8][2]int
+}
+
+func (g *Grid) Display() {
+	for _, r := range g.Board {
+		for _, c := range r {
+			fmt.Print(c)
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+	fmt.Println("Flashes:", g.Flashes)
 }
