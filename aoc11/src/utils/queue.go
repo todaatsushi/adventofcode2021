2244dcf49c0b724417595ba65bb524d1184384b1
Atsushi Toda <todaatsushi0@gmail.com>
2022-10-02 17:05:33 +0100

    Add custom queue struct to manage bfs

---
+++ aoc11/src/utils/queue.go
@@ -0,0 +1,36 @@
+package utils
+
+import "errors"
+
+type Queue struct {
+	elems [][2]int
+}
+
+func (q *Queue) Add(elem [2]int) {
+	q.elems = append(q.elems, elem)
+}
+
+func (q *Queue) Next() ([2]int, error) {
+	if q.IsEmpty() == true {
+		// Find better way of returning null val
+		nullValue := [2]int{-1, -1}
+		return nullValue, errors.New("Queue is empty!")
+	}
+	elem := q.elems[0]
+	q.elems = q.elems[1:]
+	return elem, nil
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.elems) == 0
+}
+
+func NewQueue() Queue {
+	elems := make([][2]int, 0)
+	for r := 0; r < 10; r++ {
+		for c := 0; c < 10; c++ {
+			elems = append(elems, [2]int{r, c})
+		}
+	}
+	return Queue{elems: elems}
+}
