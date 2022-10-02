cbc913014cd6aa58f6620b40bc4f2da8d3a638c3
Atsushi Toda <todaatsushi0@gmail.com>
2022-10-02 17:07:17 +0100

    Generate valid moves for each flash using the modifiers and checking that the new coords are in bounds

--- aoc11/src/grid/struct.go
+++ aoc11/src/grid/struct.go
@@ -10,6 +10,25 @@ type Grid struct {
 	coordinateModifiers [8][2]int
 }
 
+func (g *Grid) getMoves(current [2]int) [][2]int {
+	moves := make([][2]int, 0)
+	var newRow, newCol int
+
+	for _, modifier := range g.coordinateModifiers {
+		newRow = current[0] + modifier[0]
+		newCol = current[1] + modifier[1]
+
+		if g.validCoordinates(newRow, newCol) {
+			moves = append(moves, [2]int{newRow, newCol})
+		}
+	}
+	return moves
+}
+
+func (g *Grid) validCoordinates(row, col int) bool {
+	return row < len(g.Board) && col < len(g.Board[0]) && row >= 0 && col >= 0
+}
+
 func (g *Grid) Display() {
 	for _, r := range g.Board {
 		for _, c := range r {
