f8e223ddee881c610d04119ce9379ee03fb719d6
Atsushi Toda <todaatsushi0@gmail.com>
2022-10-02 17:22:10 +0100

    Solve part 1

--- aoc11/main.go
+++ aoc11/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"adventofcode2021/aoc11/src/grid"
 	"adventofcode2021/aoc11/src/utils"
 	"fmt"
 )
 
 func main() {
-	filepath := "inputs/test.txt"
-	grid := utils.GetGridFromFile(filepath)
+	filepath, numSteps, display := utils.GetArgs()
 
-	fmt.Println(grid)
+	grid := grid.GetGridFromFile(filepath)
+	grid.FlashXSteps(numSteps, display)
+	if display == false {
+		fmt.Println("Flashes:", grid.Flashes)
+	}
 }
