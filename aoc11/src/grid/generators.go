0375eb2301ad0bdce15256afeffe16ad997f9f13
Atsushi Toda <todaatsushi0@gmail.com>
2022-10-02 16:28:18 +0100

    Move grid makers to grid packages and into its own file

---
+++ aoc11/src/grid/generators.go
@@ -0,0 +1,33 @@
+package grid
+
+import (
+	"adventofcode2021/aoc11/src/utils"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func newGrid(board [10][10]int) Grid {
+	grid := Grid{board: board}
+	return grid
+}
+
+func GetGridFromFile(filepath string) Grid {
+	contentStr := utils.ReadFile(filepath)
+	lines := strings.Split(contentStr, "\n")
+
+	var board [10][10]int
+	for row, line := range lines {
+		octopuses := strings.Split(line, "")
+		for col, octopus := range octopuses {
+			val, err := strconv.Atoi(octopus)
+			if err != nil {
+				log.Fatal("Couldn't convert value to int:", octopus)
+			}
+			board[row][col] = val
+		}
+	}
+
+	grid := newGrid(board)
+	return grid
+}
--- aoc11/src/utils/parse.go
+++ aoc11/src/utils/parse.go
@@ -3,13 +3,10 @@ package utils
 import (
 	"log"
 	"os"
-	"strconv"
 	"strings"
-
-	"adventofcode2021/aoc11/src/grid"
 )
 
-func readFile(filepath string) string {
+func ReadFile(filepath string) string {
 	content, err := os.ReadFile(filepath)
 	if err != nil {
 		log.Fatal("Couldn't read file! Path: ", filepath)
@@ -18,24 +15,4 @@ func readFile(filepath string) string {
 	contentStr := string(content)
 	contentStr = strings.TrimSpace(contentStr)
 	return contentStr
-}
-
-func GetGridFromFile(filepath string) grid.Grid {
-	contentStr := readFile(filepath)
-	lines := strings.Split(contentStr, "\n")
-
-	var board [10][10]int
-	for row, line := range lines {
-		octopuses := strings.Split(line, "")
-		for col, octopus := range octopuses {
-			val, err := strconv.Atoi(octopus)
-			if err != nil {
-				log.Fatal("Couldn't convert value to int:", octopus)
-			}
-			board[row][col] = val
-		}
-	}
-
-	grid := grid.NewGrid(board)
-	return grid
 }
