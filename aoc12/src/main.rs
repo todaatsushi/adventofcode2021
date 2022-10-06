8a6cf1fa4701dbde3dd19a1a595779ef80f4475a
Atsushi Toda <todaatsushi0@gmail.com>
2022-10-06 13:14:00 +0100

    Solve part 1

--- aoc12/src/main.rs
+++ aoc12/src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+use aoc12::input;
+use aoc12::solve;
+
+fn main() {
+    let caves = input::parse_file_to_map();
+    let num_paths = solve::get_num_paths(caves);
+
+    println!("{:?}", num_paths);
+}
