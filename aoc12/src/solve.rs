bf5066d5a6170ce7a67dd563c3e228c427cbe613
Atsushi Toda <todaatsushi0@gmail.com>
2022-10-06 13:13:53 +0100

    DFS to solve part 1

---
+++ aoc12/src/solve.rs
@@ -0,0 +1,33 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+
+pub fn get_num_paths(caves: HashMap<&str, Vec<&str>>) -> u32 {
+    let mut num_paths: u32 = 0;
+    let mut queue: VecDeque<(&str, HashSet<&str>, bool)> = VecDeque::new();
+
+    let mut init_set = HashSet::new();
+    init_set.insert("start");
+    queue.push_back(("start", init_set, false));
+
+    while queue.is_empty() == false {
+        let (current, seen, visited) = queue.pop_front().unwrap();
+        if current == "end" {
+            num_paths += 1;
+            continue;
+        }
+
+        let next_caves = caves.get(&current.clone()).unwrap();
+        for cave in next_caves {
+            let seen_cave = seen.contains(cave);
+            let mut now_seen = seen.clone();
+
+            if seen_cave == false {
+                if cave.chars().all(|c| c.is_lowercase()) == true {
+                    now_seen.insert(cave);
+                }
+                queue.push_front((cave, now_seen, visited));
+            }
+        }
+    }
+
+    num_paths
+}
