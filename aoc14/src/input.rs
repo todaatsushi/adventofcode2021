5c70bb3d76022f96d2e91b8a5fa3d0b8bb6202f1
Atsushi Toda <todaatsushi0@gmail.com>
2022-10-08 17:35:31 +0100

    init get file content func

---
+++ aoc14/src/input.rs
@@ -0,0 +1,22 @@
+use std::env;
+
+#[derive(Debug)]
+enum InputError {
+    FileDoesntExist,
+    Needs2Args,
+}
+
+fn read_file() -> &'static str {
+    let args: Vec<String> = env::args().collect();
+    let content = match args.len() as u8 {
+        2 => match args[1].as_str() {
+            "test" => Ok(include_str!("../inputs/test.txt")),
+            "puzzle" => Ok(include_str!("../inputs/puzzle.txt")),
+            _ => Err(InputError::FileDoesntExist),
+        },
+        _ => Err(InputError::Needs2Args),
+    }
+    .unwrap();
+
+    content
+}
